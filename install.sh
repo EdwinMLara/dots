50f0d70d143bc93385b761738a1a0b7e898736da
EdwinMLara <emlara35@gmail.com>
2026-09-07 18:30:52 -0600

    feat(install): clonar TPM automaticamente en la instalacion
    se agrega la clonacion de tmux-plugins/tpm en install.sh para el gestor
    de plugins de tmux quede listos sin pasos manuales adicionales.

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 echo "Configurando Tmux..."
 ln -sf ~/.config/nvim/tmux/tmux.conf ~/.tmux.conf
 
+# Instalar TPM (gestor de plugins de tmux) si no existe
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  echo "Instalando TPM (tmux plugin manager)..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 # Instalar dependencias de Homebrew
 echo "Instalando dependencias desde el Brewfile..."
 brew bundle --file=~/.config/nvim/Brewfile
